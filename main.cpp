7ad88dec0e583af0ec82b867309ed7efe0747ef3
Cunev <cunev2005@gmail.com>
2025-02-24 21:14:22 +0300

    Add lost files.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,9 @@
 #include <cstring>
 #include <iostream>
-
-int RunInteractive() { return 0; }
-
-int RunDemo() { return 0; }
+#include "MyStack/MyStack.h"
 
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "Russian");
 
-    bool isInteractive = false;
-
-    if ((argc == 2) && strcmp(argv[1], "i") == 0) {
-        isInteractive = true;
-    }
-
-    if (isInteractive) {
-        return RunInteractive();
-    } else {
-        return RunDemo();
-    }
+    return MyStack::RunTask();
 }
